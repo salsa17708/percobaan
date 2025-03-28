b99c2aef267ea0dcde674e69c5ee85e65ea7a858
salsa17708 <salsabilaputri17708@gmail.com>
2025-03-28 21:26:35 +0700

    5.3. menyesuaikan userseeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +12,6 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        User::factory()->count(5)->create();
     }
 }
