ee8c23836f8d679f089bf81c889c7b414aecd214
salsa17708 <salsabilaputri17708@gmail.com>
2025-03-28 21:33:20 +0700

    memanggil seeder di database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,13 +11,20 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UserSeeder::class,
+            CategorySeeder::class,
+            CourseSeeder::class,
         ]);
+
+         User::factory()->create([
+            'name' => 'salsa',
+            'email' => 'sall@example.com',
+        ]);
+        
+
     }
 }
