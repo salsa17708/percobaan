51b4ec6612fd292596eae050462a71cdbfe0fa44
salsa17708 <salsabilaputri17708@gmail.com>
2025-03-28 20:23:01 +0700

    3.menyesuaikan model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $fillable = ['title', 'description', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongTo(Category::class);
+    }
 }
--- database/migrations/2025_03_28_124548_create_courses_table.php
+++ database/migrations/2025_03_28_124548_create_courses_table.php
@@ -4,23 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+
+return new class extends Migration {
+    public function up()
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('category_id');
             $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('courses');
     }
