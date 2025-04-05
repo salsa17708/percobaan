190d41471b865f42f0d6e73a7513fb620d82361a
salsa17708 <salsabilaputri17708@gmail.com>
2025-04-05 08:52:38 +0700

    revisi

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,7 +14,6 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => bcrypt('password'),
             'remember_token' => Str::random(10),
         ];
